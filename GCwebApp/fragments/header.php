d8a7d29a705b32260be3383f0c3d16a1567af6bd
Reuel M. Bigo <2121855@slu.edu.ph>
2017-12-28 09:46:18 +0800

    Add files via upload

---
+++ GCwebApp/fragments/header.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a href="#" class="navbar-brand">Goldust Creations</a>
+		</div>
+
+		
+		
+	</div>
+	
+</nav>
---
+++ GCwebApp/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php 
+		include 'fragments/head.php';
+	?>
+	<body>
+
+		<section class="jumbotron text-center">
+			<h1>Goldust Creations</h1>
+			<p>We Specialize in Event Management</p>
+		</section>
+		
+	</body>
+</html>
